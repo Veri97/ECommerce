5ac336df194e0096f8d1be18c79180cb9b7da25c
Veri97 <veri.kazazi1997@hotmail.com>
2022-02-17 18:54:17 +0100

    removed boilerplate code

---
+++ ECommerce.API/Controllers/BaseECommerceController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseECommerceController : ControllerBase { }
+
---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers;
+public class ProductsController : BaseECommerceController
+{
+    [HttpGet]
+    public string GetAllProducts()
+    {
+        return "This will be a list of products";
+    }
+
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+        return $"single product - {id}";
+    }
+}
+
