7bfe7a379afcba15a8bced385ec13ea40bf495e5
Veri97 <veri.kazazi1997@hotmail.com>
2022-02-28 22:53:08 +0100

    ECommerce.Core and ECommerce.Infrastructure .csproj

---
+++ ECommerce.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Data;
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
+
