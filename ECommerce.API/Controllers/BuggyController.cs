174c4793420d9b84e8b2564f385878e8b3fa41b7
Veri97 <veri.kazazi1997@hotmail.com>
2022-07-27 17:25:40 +0200

    fix: added error interceptor in client project

---
+++ ECommerce.API/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+﻿using ECommerce.API.Errors;
+using ECommerce.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    public class BuggyController : BaseECommerceController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+
+            if (thing == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400, "you have made a bad request!"));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
--- ECommerce.WebApp/src/app/core/core.module.ts
+++ ECommerce.WebApp/src/app/core/core.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavBarComponent} from "./nav-bar/nav-bar.component";
 import {RouterModule} from "@angular/router";
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import {ToastrModule} from "ngx-toastr";
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+       positionClass: 'toast-bottom-right',
+       closeButton: true,
+       preventDuplicates: true,
+       timeOut: 3000,
+       iconClasses: {
+        error: 'toast-error',
+        info: 'toast-info',
+        success: 'toast-success',
+        warning: 'toast-warning'
+      }
+    })
   ],
   exports: [NavBarComponent]
 })
