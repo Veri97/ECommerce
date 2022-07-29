d44b49e36544856518cb6fc96f6f6075f6eb4b12
Veri97 <veri.kazazi1997@hotmail.com>
2022-07-29 18:59:24 +0200

    feat(client-basket): started to setup basket service and routing in frontend

--- ECommerce.WebApp/src/app/app-routing.module.ts
+++ ECommerce.WebApp/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 import {TestErrorComponent} from "./core/test-error/test-error.component";
 import {ServerErrorComponent} from "./core/server-error/server-error.component";
 import {NotFoundComponent} from "./core/not-found/not-found.component";
@@ -10,7 +10,16 @@ const routes: Routes = [
   {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
-  {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
+    data: {breadcrumb: 'Shop'}
+  },
+  {
+    path: 'basket',
+    loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+    data: {breadcrumb: 'Basket'}
+  },
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
 ];
 
@@ -18,4 +27,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
